3ada6dc9e21bc23a192474f04083b75812124d93
krunaltelang01@gmail.com <Admin@DESKTOP-8BLGLA0>
2020-10-11 16:09:38 +0530

    hey qa i Update script as per ur instruction

--- GitDEMO_First/src/first/First.java
+++ GitDEMO_First/src/first/First.java
@@ -6,17 +6,20 @@ public class First {
 	{
 		System.out.println("Write code for Covide vaccine");
 		
-		int age= 18;
+		int age=4;
 		
 		if(age>18) 
 		{
 			System.out.println("Covide 19 candidate is applicable for vacacine");
 		}
+		else if(age >=18) 
+		{
+			System.out.println("Covide 19 is candidate Sould take vaccine");
+		}
 		else 
 		{
-			System.out.println("Covide 19 is candidate Not applicable for vacacine");
+			System.out.println("VAccine jar nahi  ghetli tr tu mela");
 		}
-		
 
 	}
 
