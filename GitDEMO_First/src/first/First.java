a169761a433dc7b3de23af2aa42e4b99a63f0dfb
krunaltelang01@gmail.com <Admin@DESKTOP-8BLGLA0>
2020-10-11 15:56:31 +0530

    Hey TL I complete my given Script

--- GitDEMO_First/src/first/First.java
+++ GitDEMO_First/src/first/First.java
@@ -5,6 +5,18 @@ public class First {
 	public static void main(String[] args) 
 	{
 		System.out.println("Write code for Covide vaccine");
+		
+		int age= 18;
+		
+		if(age>18) 
+		{
+			System.out.println("Covide 19 candidate is applicable for vacacine");
+		}
+		else 
+		{
+			System.out.println("Covide 19 is candidate Not applicable for vacacine");
+		}
+		
 
 	}
 
